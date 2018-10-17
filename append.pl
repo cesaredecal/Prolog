3bc4c1246e1ffeb10cf2d4e391bbc4b1ee4b3cf1
Cesare de Cal <hi@cesare.io>
2018-10-17 12:04:33 +0200

    Create append.pl

---
+++ append.pl
@@ -0,0 +1,12 @@
+: Prolog -*-
+%!  Inizio file
+
+% ! Programma che date due liste le concatena restituendo la lista
+% concatenata
+
+appendi([], L, L).
+
+appendi((X | L1), L2, (X | L3)) :-
+    appendi(L1, L2, L3).
+
+%% Fine file
