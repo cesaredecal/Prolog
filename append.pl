2b76ce079c5f9e18abec508aaec2f08d5019f655
Cesare de Cal <hi@cesare.io>
2018-10-17 12:07:31 +0200

    Update append.pl

--- append.pl
+++ append.pl
@@ -4,8 +4,10 @@
 % ! Programma che date due liste le concatena restituendo la lista
 % concatenata
 
+% Caso base
 appendi([], L, L).
 
+% Passo ricorsivo
 appendi((X | L1), L2, (X | L3)) :-
     appendi(L1, L2, L3).
 
